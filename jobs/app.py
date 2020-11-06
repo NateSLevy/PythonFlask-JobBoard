33a62197d406578a4ea0f7222170a682d13822d4
NateSLevy <74020754+NateSLevy@users.noreply.github.com>
2020-11-06 21:21:41 +0200

    P6 1.0

--- jobs/app.py
+++ jobs/app.py
@@ -27,6 +27,12 @@ def execute_sql(sql, values = (), commit = False, single = False):
 
     return results
 
+def job(job_id):
+    job = execute_sql(SELECT job.id, job.title, job.description, job.salary,
+    employer.id as employer_id, employer.name as employer_name FROM job JOIN
+    employer ON employer.id = job.employer_id WHERE job.id = ?', [job_id], single=True)
+    return render_template('job.html', job = job)
+
 @app.teardown_appcontext
 def close_connection(exception):
     connection = getattr(g, '_connection', None)
@@ -36,6 +42,7 @@ def close_connection(exception):
 
 @app.route("/")
 @app.route("/jobs")
+@app.route("/job/<job_id>")
 def jobs():
     jobs = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id')
     return render_template('index.html', jobs = jobs)
